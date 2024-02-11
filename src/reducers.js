2c8b0b1050f57b2041788326885e465aa49e80a9
ChepkemoiS <memoi24sharon@gmail.com>
2024-02-12 03:13:46 +0630

    completed

--- src/reducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import transactionsReducer from './reducers/transactionsReducer';
-
-const rootReducer = combineReducers({
-  transactions: transactionsReducer
-});
-
-export default rootReducer;
